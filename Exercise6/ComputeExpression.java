9cca60d6606c97614278c7cbfcbe1cf53729fbcb
Kieu Anh <keahdolly1703@gmail.com>
2024-09-09 13:46:09 +0700

    Add files via upload

---
+++ Exercise6/ComputeExpression.java
@@ -0,0 +1,6 @@
+public class ComputeExpression {
+    public static void main(String[] args) {
+        System.out.println((12.5 * 5.0 + 5.5) / (4.5 + 2.0 * 3.5));
+        System.out.println((12.5 * 5.0 + 5.5) / (4.5 + 2.0 * 3.5));
+    }
+}
